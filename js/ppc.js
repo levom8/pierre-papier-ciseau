3a661d5c43dc5cac8ea6ab961cf442857244b9c3
levom8 <levom8@gmail.com>
2021-12-05 23:00:46 -0500

    process before changing functions

--- js/ppc.js
+++ js/ppc.js
@@ -1,20 +1,29 @@
 let computerSelection;
 let playerSelection;
-let lostRound;
-let wonRound;
-let tieRound;
 let playerScore = 0;
 let computerScore = 0;
+let choix = prompt("Pick either Pierre, Papier ou Ciseau")
 
-function game(input) {
-
-        for (i = 0; i <= 5; i++) {
-            let playerSelection = prompt("Please choose between Pierre, Papier ou Ciseau");
-            
-            if (playerSelection )
-        
-        }
+function computerPlay() {
+    let arr = ["Papier","Pierre","Ciseau"]
+    computerSelection = arr[Math.floor(Math.random() * arr.length)];
+    return computerSelection;
 }
+
+function playerPlay(hand) {
+    
+    let capLetter = hand.slice(0,1).toUpperCase();
+    let smallLetter = hand.slice(1).toLowerCase();
+    
+    playerSelection = capLetter + smallLetter;
+
+    if (!(playerSelection == "Papier" || playerSelection == "Pierre" || playerSelection == "Ciseau")) {
+        alert("ERROR, type either \"Papier\", \"Pierre\" ou \"Ciseau\"");
+    }
+    else {
+        return(playerSelection);
+    }
+    }
 
 function playRound(computerSelection, playerSelection) {
     if (computerSelection == "Papier" && playerSelection == "Pierre" || computerSelection == "Pierre" && playerSelection == "Ciseau" || computerSelection == "Ciseau" && playerSelection == "Papier") {
@@ -31,28 +40,13 @@ function playRound(computerSelection, playerSelection) {
     }
 }
 
-function computerPlay() {
-    let arr = ["Papier","Pierre","Ciseau"]
-    computerSelection = arr[Math.floor(Math.random() * arr.length)];
-    return computerSelection;
+function game(input) {
+    console.log(input);
+    console.log(input);
+    console.log(input);
+    console.log(input);
 }
 
-
-function playerPlay(hand) {
-    let capLetter = hand.slice(0,1).toUpperCase();
-    let smallLetter = hand.slice(1).toLowerCase();
-    
-    playerSelection = capLetter + smallLetter;
-
-    if (!(playerSelection == "Papier" || playerSelection == "Pierre" || playerSelection == "Ciseau")) {
-        alert("ERROR, type either \"Papier\", \"Pierre\" ou \"Ciseau\"");
-    }
-    else {
-        return(playerSelection);
-    }
-    }
-
 computerPlay();
-playerPlay("pieRRE");
-playRound(computerSelection, playerSelection);
+playerPlay(choix);
 game(playRound(computerSelection, playerSelection));
